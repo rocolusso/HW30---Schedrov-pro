c287dab7ff9d2da739601d3777306980206f087a
schedrov <rocolusso@icloud.com>
2022-05-31 16:34:30 +0300

    complete1

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
- 'use strict';
-
+'use strict';
 /*
-
 Написать свою реализацию функции bind()
-
-
  */
 
- void function (){
+void function () {
+    const car = {make: 'Nissan', year: 3.7};
 
-     const car = {make: 'Nissan', year: 3.7 };
+    function carData(make, year, engine) {
+        console.log(`make: ${this.make} \nyear: ${+year} \nengine: ${+engine} `);
+    }
 
-     function carData (make, year, engine) {
-         console.log(`make: ${this.make} \nyear: ${+year} \nengine: ${+engine} `);
-     }
+    function bind(func, ctx) {
+        return function (argggs) {
+            const args = [].slice.call(arguments);
+            return func.apply(ctx, args);
+        };
+    }
 
-     function bind  (func, ctx) {
-         return function () {
-             const params = [].slice.call(arguments);
-             return func.apply(ctx, params);
-         };
-     }
+    bind(carData, car)('Nissan', 2020, 3.7);
 
-     bind(carData, car)('Nissan', 2020 , 3.7);
-
- }();
+}();
 
 
